f28ea5ba1565334b48c123313e703fae3fa06604
C.Rud Glasdam <cecilie.rud@hotmail.com>
2025-03-25 16:04:33 +0100

    MenuTilføjet

--- Genspilv2/Genspilv2/Program.cs
+++ Genspilv2/Genspilv2/Program.cs
@@ -4,7 +4,106 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Indsættelse af variabler der kan kaldes på i alle menuerne
+
+
+            //Menu i Main
+            static void Main(string[] args)
+            {
+                bool showMenu = true;
+                while (showMenu)
+                {
+                    showMenu = MainMenu();
+                }
+            }
+        }
+
+        private static bool MainMenu()
+        {
+            Console.WriteLine("""
+            Vælg punkt: 
+            1) Overblik over spil
+            2) Reservation af spil
+            3) Søge på spil
+            4) Efterspørgelse af spil
+            5) Prissættelse af spil
+            6) Lagerliste
+            7) Afslut programmet
+            """);
+
+            //Debug sikring for at sikre sig, at man er hvor man ønsker
+            string valg = Console.ReadLine();
+            Console.WriteLine(valg);
+
+            //cases på menuen
+            switch (valg)
+            {
+                case "1":
+                    Overblik();
+                    return true;
+                case "2":
+                    Reservation();
+                    return true;
+                case "3":
+                    Soegning();
+                    return true;
+                case "4":
+                    Efterspoergelse();
+                    return true;
+                case "5":
+                    Prissaettelse();
+                    return true;
+                case "6":
+                    Lagerliste();
+                    return true;
+                case "7":
+                    Afslut();
+                    return false;
+                default:
+                    return true;
+            }
+        }
+        //Case 1 Overblik
+        private static void Overblik()
+        {
+            Console.WriteLine("Her er Overblik");
+        }
+
+        //Case 2 Reservation
+        private static void Reservation()
+        {
+            Console.WriteLine("Her er Reservation");
+        }
+
+        //Case 3 Søge
+        private static void Soegning()
+        {
+            Console.WriteLine("Her er Søge");
+        }
+
+        //Case 4, Efterspørgelse
+        private static void Efterspoergelse()
+        {
+            Console.WriteLine("Her er Efterspørgelse");
+        }
+
+        //Case 5, Prissættelse
+        private static void Prissaettelse()
+        {
+            Console.WriteLine("Her er Prissættelse");
+        }
+
+        //Case 6, Lagerliste,
+        private static void Lagerliste()
+        {
+            Console.WriteLine("Her er Lagerlisten");
+        }
+
+        //Case 7, Afslut
+        private static void Afslut()
+        {
+            Console.WriteLine($"\r\nTryk Enter for at Afslutte");
+            Console.ReadLine();
         }
     }
 }
