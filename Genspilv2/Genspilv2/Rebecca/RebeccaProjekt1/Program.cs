03cdb82060d17e06c72c5d0b36d272b9bcdf554d
Rebecca P <rebecca_pedersen@msn.com>
2025-03-26 10:18:39 +0100

    tilføjede Rebecca fil

---
+++ Genspilv2/Genspilv2/Rebecca/RebeccaProjekt1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RebeccaProjekt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
